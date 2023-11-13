762f2ec10b89b8d3c2153013d9c6fb11cb3ff3dd
PatricaM <patriciadeleon02@gmail.com>
2023-11-13 11:38:39 -0800

    actualizacion

---
+++ app/src/main/java/com/example/gym/NavBotDialog.java
@@ -0,0 +1,14 @@
+package com.example.gym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NavBotDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_bot_dialog);
+    }
+}
